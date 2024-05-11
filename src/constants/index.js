c100c6ef038580eaab984486e6569294dc75afbc
Derkaoui05 <derkaouidev@gmail.com>
2024-05-11 21:40:28 +0100

    finaalll

--- src/constants/index.js
+++ src/constants/index.js
@@ -20,28 +20,28 @@ export const EXPERIENCES = [
   {
     year: "2023 - Present",
     role: "Senior Full Stack Developer",
-    company: "Google Inc.",
+    company: "2MAG Agency",
     description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
     technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
   },
   {
     year: "2022 - 2023",
     role: "Frontend Developer",
-    company: "Adobe",
+    company: "Google Inc",
     description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
     technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
   },
   {
     year: "2021 - 2022",
     role: "Full Stack Developer",
-    company: "Facebook",
+    company: "Vercel",
     description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+    technologies: ["Python", "Svelte", "Three.js", "MongoDb"],
   },
   {
     year: "2020 - 2021",
     role: "Software Engineer",
-    company: "Paypal",
+    company: "Ibm",
     description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
     technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
   },
@@ -53,7 +53,7 @@ export const PROJECTS = [
     image: project1,
     description:
       "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+    technologies: ["HTML", "CSS", "React", "MySql"],
   },
   {
     title: "Task Management App",
@@ -74,7 +74,7 @@ export const PROJECTS = [
     image: project4,
     description:
       "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+    technologies: ["HTML", "CSS", "React", "mySQL"],
   },
 ];
 

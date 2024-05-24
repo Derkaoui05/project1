b969f7b332c93af47e198014d1d304ff7d97355c
Derkaoui05 <derkaouidev@gmail.com>
2024-05-24 07:39:12 +0100

    contactc

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,19 +6,19 @@ import { FaLinkedin } from 'react-icons/fa'
 const Footer = () => {
     return (
         <>
-            <div id="works" className="border-b border-neutral-900 pb-5">
-                <div className="h-full flex flex-col gap-8 items-center justify-between p-10 sm:p-7">
+            <div className="border-b border-neutral-900 pb-5">
+                <div className="h-full flex flex-col  gap-8 items-center justify-between p-10 sm:p-7">
                     <motion.h2 whileInView={{ opacity: 1, y: 0 }}
                         initial={{ opacity: 0, y: -100 }}
                         transition={{ duration: .5 }}
                         className=' text-center text-4xl text-neutral-500'>Let's Talk</motion.h2>
                     <div className=" flex items-center justify-center gap-8 sm:gap-5">
-                        <motion.a
+                        <motion.a 
                         whileInView={{opacity:1,x:0}}
                         initial={{opacity:0,x:-100}}
                         transition={{duration:.5}}
                             href="https://www.linkedin.com/in/derkaoui-yassir-497b5a298/"
-                            className="box font-medium text-white   flex items-center justify-center flex-col"
+                            className="box font-medium text-white flex items-center justify-center gap-3 flex-col md:flex-row"
                         >
                             <FaLinkedin className=" text-white text-3xl hover:scale-125 cursor-pointer" />
                             <p>Linkedin</p>
@@ -28,7 +28,7 @@ const Footer = () => {
                            initial={{opacity:0,x:-100}}
                            transition={{duration:1}}
                             href="https://github.com/Derkaoui05"
-                            className="box font-medium text-white  flex items-center justify-center flex-col"
+                            className="box font-medium text-white  flex items-center justify-center gap-3 flex-col md:flex-row"
                         >
                             <IoLogoGithub className=" text-white text-3xl hover:scale-125 cursor-pointer" />
                             <p>Github</p>
@@ -38,7 +38,7 @@ const Footer = () => {
                            initial={{opacity:0,x:-100}}
                            transition={{duration:1}}
                             href="https://www.instagram.com/d.ysr777"
-                            className="box font-medium text-white  flex items-center justify-center flex-col"
+                            className="box font-medium text-white  flex items-center justify-center gap-3 flex-col md:flex-row"
                         >
                             <RiInstagramFill className=" text-white text-3xl hover:scale-125 cursor-pointer" />
                             <p>Instagram</p>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,6 +11,7 @@ const container = (delay) => ({
         transition: { duration: 0.5, delay: delay }
     }
 })
+
 const Hero = () => {
     return (
         <>
@@ -40,7 +41,7 @@ const Hero = () => {
                         </div>
                     </div>
                     <div className="w-full lg:w-1/2 lg:p-8">
-                        <div className="flex  justify-center">
+                        <div className="flex justify-center">
                             <motion.img
                                 initial={{ x: 100, opacity: 0 }}
                                 animate={{ x: 0, opacity: 1 }}

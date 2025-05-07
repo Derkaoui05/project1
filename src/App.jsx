f2e31a69dfe5d33a254ff8e7e8579b5262a66421
Derkaoui05 <147924188+Derkaoui05@users.noreply.github.com>
2025-05-07 21:53:35 +0100

    adjust navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Experience from "./components/Experience"
 import Footer from "./components/Footer"
 import Hero from "./components/Hero"
 import Menu from "./components/Menu"
-import Navbar from "./components/Navbar"
 import Projects from "./components/Projects"
 import Skills from "./components/Skills"
 

a36936a1b0e0ee94152cff72d6f1c323edd55762
Derkaoui05 <147924188+Derkaoui05@users.noreply.github.com>
2025-12-25 17:20:59 +0100

    add git and docker to skills

--- src/components/sections/Skills.jsx
+++ src/components/sections/Skills.jsx
@@ -6,7 +6,7 @@ import {
     RiTailwindCssLine,
     RiWordpressLine
 } from 'react-icons/ri'
-import {SiLaravel, SiMysql, SiShadcnui, SiSpring, SiTypescript} from 'react-icons/si'
+import {SiDocker, SiGit, SiLaravel, SiMysql, SiShadcnui, SiSpring, SiTypescript} from 'react-icons/si'
 import {motion} from 'framer-motion'
 
 const iconVariants = (duration, delay, amplitude) => ({
@@ -55,14 +55,16 @@ const Skills = () => {
                     { Icon: RiWordpressLine, duration: 2.8, delay: 0.8, amplitude: 16 },
                     { Icon: SiSpring, duration: 3, delay: 1, amplitude: 14 },
                     { Icon: SiLaravel, duration: 3.2, delay: 1.2, amplitude: 17 },
-                    { Icon: SiMysql, duration: 3.4, delay: 1.4, amplitude: 13 }
+                    { Icon: SiMysql, duration: 3.4, delay: 1.4, amplitude: 13 },
+                    { Icon: SiGit, duration: 3.6, delay: 1.6, amplitude: 15 },
+                    { Icon: SiDocker, duration: 3.8, delay: 1.8, amplitude: 18 }
                 ].map(({ Icon, duration, delay, amplitude }, index) => (
                     <motion.div
                         key={index}
                         variants={iconVariants(duration, delay, amplitude)}
                         initial="initial"
                         animate="animate"
-                        className="rounded-2xl border-4 border-neutral-800 p-4"
+                        className="rounded-2xl  border-4 border-neutral-800 p-4"
                     >
                         <Icon className='text-7xl text-cyan-400' />
                     </motion.div>

97c5e3a63f88fac658d8df7631b82bb7004329fc
Derkaoui05 <147924188+Derkaoui05@users.noreply.github.com>
2026-05-05 11:59:49 +0100

    add admin dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -11,34 +11,49 @@ import WorkProcess from './components/sections/Process';
 import Projects from './components/sections/Projects';
 import Skills from './components/sections/Skills';
 import ContactForm from './components/ui/ContactForm';
+import { Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/admin/Index';
 
 const App = () => {
   const aboutRef = useRef(null);
   const isAboutInView = useInView(aboutRef, { margin: "-30% 0px -30% 0px" });
 
   return (
-    <LayoutGroup>
-      <div className="overflow-x-hidden text-neutral-300 antialiased font-mona-sans relative">
+    <>
+     <Routes>
+  <Route
+    path="/"
+    element={
+      <LayoutGroup>
+        <div className="overflow-x-hidden text-neutral-300 antialiased font-mona-sans relative">
           <div className="fixed top-0 -z-10 h-full w-full">
             <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
           </div>
-        <AnimatedMenuDemo />
-        <div className="container mx-auto px-8 pt-36">
-          <Hero isImageHere={!isAboutInView} />
-          <div ref={aboutRef}>
-            <About isImageHere={isAboutInView} />
+
+          <AnimatedMenuDemo />
+
+          <div className="container mx-auto px-8 pt-36">
+            <Hero isImageHere={!isAboutInView} />
+            <div ref={aboutRef}>
+              <About isImageHere={isAboutInView} />
+            </div>
+            <Skills />
+            <Experience />
+            <Education />
+            <Projects />
+            <Pricing />
+            <WorkProcess />
+            <ContactForm />
+            <Footer />
           </div>
-          <Skills />
-          <Experience />
-          <Education />
-          <Projects />
-          <Pricing />
-          <WorkProcess />
-          <ContactForm />
-          <Footer />
         </div>
-      </div>
-    </LayoutGroup>
+      </LayoutGroup>
+    }
+  />
+
+  <Route path="/admin" element={<Dashboard />} />
+</Routes>
+      </>
   );
 };
 
